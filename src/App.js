b92f8c424679d4f948b1d8cc368dee7bdfcf01b7
Ducpham <ducpv@beetsoft.com>
2023-02-27 13:07:53 +0700

    pull code

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import Header from './components/Header';
 import Sinhvien from './components/Sinhvien';
 import FormEvent from './components/FormEvent';
 import FormClick from './components/FormClick';
+import FormChange from './components/FormChange';
+
 
 
 // them danh sach sinh vien
 function App() {
   return (
     <div className="App">
-      {/* <Header/>
-      <Sinhvien/> */}
+      {/* <Header/> */}
+      <Sinhvien/>
      {/* <FormEvent/> */}
      <FormClick />
+     {/* <FormChange /> */}
     </div>
   );
 }
--- src/components/FormClick.jsx
+++ src/components/FormClick.jsx
@@ -3,45 +3,47 @@ import React, { useState } from 'react'
 export default function FormClick() {
 
     const [menu, setMenu] = useState([
-        {
-          id: 1,
-          name: "Tab 1 ",
-          link: "/home",
-        },
-        {
-          id: 2,
-          name: "Tab 2 ",
-          link: "/qc",
-        },
-        {
-          id: 3,
-          name: "Tabv 3 ",
-          link: "/st",
-        },
-      ]);
+      {
+        id: 1,
+        name: "Tab 1 ",
+        link: "/home",
+      },
+      {
+        id: 2,
+        name: "Tab 2 ",
+        link: "/qc",
+      },
+      {
+        id: 3,
+        name: "Tabv 3 ",
+        link: "/st",
+      },
+    ]);
+
     const listContent = [
-        { id: 1, content: "THang" },
-        { id: 2, content: "Duc" },
-        { id: 3, content: "suc" },
-      ];
+      { id: 1, content: "THang" },
+      { id: 2, content: "Duc" },
+      { id: 3, content: "suc" },
+    ];
       //  -> find :  { }   1 phan tu   ->   content.id  === Click.id
       //  -> Map:   [ ]   3 phan tu
       //  -> filter:  [ ]   < =  3 phan tu
       //  -> forEach   ->  underfine
     
       const [itemClick, setItemClick] = useState(); // 3
+      console.log(itemClick);
       const onClickItem = (data) => {
+        // console.log(data);
         // lay du lieu click
-        setItemClick(data);
-    
+        setItemClick(data);    
         // xoa phan tu 3 :
         // const newMenu = menu.filter((m) => m.i
-    }
+      }
     // const newRenderMenu = menu.filter((m) => m.id !== itemClick?.id); //  1 -2
     // const menu = menu.filter((m) => m.id !== itemClick?.id); //  1 -2
     return (
         <div style={{ display: "flex", margin: "20px" }}>
-        {menu?.map((dataItem) => (
+        {menu?.map((dataItem) => (          
             <div
             onClick={() => onClickItem(dataItem)}
             key={dataItem?.id}
