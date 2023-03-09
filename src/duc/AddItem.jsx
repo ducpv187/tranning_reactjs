6e182132c89f4bcf0fb98ac2320cedb4fd4b8077
Truong Duc Thang <61042675+truongducthang@users.noreply.github.com>
2023-03-09 22:19:44 +0700

    Add props

--- src/duc/AddItem.jsx
+++ src/duc/AddItem.jsx
@@ -1,27 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+
 
 
 export default function AddItem(props) {
+  const [valueInput, setValueInput] = useState();
+  console.log(valueInput);
+  const handleChangeInput = (e) => {
+    setValueInput(e.target.value); // Băt buộc
+  };
 
-    
-    const [valueInput, setValueInput] = useState();
-    console.log(valueInput);
-    const handleChangeInput = (e) => {
-        setValueInput(e.target.value)
-    }
+  const handleCLickAddItem = () => {
+    // props.setData((prevData) => {
+    //    return [...prevData , valueInput]
+    // })
 
-    const handleCLickAddItem = () => {
-        props.setData((prevData) => {
-           return [...prevData , valueInput]
-        })
-    }
-
+    props.handleCLickAddValueItem(valueInput);
+  };
 
   return (
     <div className="addItem">
-        <h1>AddItem</h1>
-        <input type="text" placeholder='Nhap ten item' onChange={handleChangeInput}/>
-        <button onclick={handleCLickAddItem}>them item</button>        
+      <h1>AddItem</h1>
+      <input
+        type="text"
+        placeholder="Nhap ten item"
+        onChange={handleChangeInput}
+      />
+      <button onclick={handleCLickAddItem}>them item</button>
     </div>
-  )
+  );
 }
--- src/duc/Crud.jsx
+++ src/duc/Crud.jsx
@@ -1,17 +1,30 @@
-import React, { useState } from 'react'
-import AddItem from './AddItem'
-import ListData from './ListData'
+import React, { useState } from "react";
+import AddItem from "./AddItem";
+import ListData from "./ListData";
 
 export default function Crud() {
-    const [data ,setData] = useState([{id:1 , name: 'duc'}, { id:2 , name: 'thang'}])
+  const [data, setData] = useState([
+    { id: 1, name: "duc" },
+    { id: 2, name: "thang" },
+  ]);
+
+  const handleCLickAddValueItem = (value) => {
+    console.log(value);
+    setData((prevData) => {
+      return [...prevData, value];
+    });
+  };
   return (
     <div>
-        <h1>CRUD</h1>
-        <AddItem />    
-        <ListData dataList={data} setData={setData}/>    
+      <h1>CRUD</h1>
+      <AddItem />
+      <ListData
+        dataList={data}
+        setData={setData}
+        handleCLickAddValueItem={handleCLickAddValueItem}
+      />
     </div>
-    
-  )
+  );
 }
 // NOTE
 
